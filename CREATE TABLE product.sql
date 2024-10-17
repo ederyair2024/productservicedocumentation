5a9b559fd4234765b9635bcadc664ee60312795d
ramonm2024 <ederyair.ramonm@hotmail.com>
2024-10-17 12:54:29 -0600

    Init branch

---
+++ CREATE TABLE product.sql
@@ -0,0 +1,1 @@
+ CREATE TABLE product (     idProduct varchar(10) NOT NULL,     name varchar(20) NOT NULL,     description varchar(200) NOT NULL,     price double NOT NULL,     model varchar(10) NOT NULL, PRIMARY KEY (idProduct) );
